8fcf80e394df2ee29d0d16a1738e5f728dd20400
kimbongsick <127704555+kimbongsick@users.noreply.github.com>
2024-09-23 19:43:48 +0900

    카드 복사
    UI 카드 오브젝트 복사기능 추가

---
+++ Assets/Scripts/CardSpawner.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardSpawner : MonoBehaviour
+{
+    public GameObject CardPrefab;
+    //private Transform canvas;
+
+    void Start()
+    {
+        //canvas = FindObjectOfType<Canvas>().transform;
+        Instantiate(CardPrefab, transform.position, Quaternion.identity, transform.parent);     // Instantiate를 부모 오브젝트에 할당
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Spwan()
+    {
+        Debug.Log("호출");
+        Instantiate(CardPrefab, transform.position, Quaternion.identity, transform.parent);     // Instantiate를 부모 오브젝트에 할당
+        //GameObject Card = Instantiate(CardPrefab, transform.position, transform.rotation);
+    }
+}
